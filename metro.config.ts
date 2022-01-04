f0e8a80cac1ae7fe59acd870bf2af3146ca94cf4
Matt Thompson <matt@echobind.com>
2022-01-04 10:31:27 -0500

    MER... apollo errors and versioning

--- metro.config.ts
+++ metro.config.ts
@@ -5,3 +5,29 @@ const defaultConfig = getDefaultConfig(__dirname);
 // defaultConfig.resolver.sourceExts.push('cjs');
 
 module.exports = defaultConfig;
+
+// import { getDefaultConfig } from 'metro-config';
+// const { resolver: defaultResolver } = getDefaultConfig.getDefaultValues();
+
+// module.exports = {
+//   ...defaultResolver,
+//   sourceExts: [...defaultResolver.sourceExts, 'cjs'],
+// };
+
+// const defaultSourceExts = require('metro-config/src/defaults/defaults').sourceExts;
+
+// module.exports = {
+//   transformer: {
+//     getTransformOptions: () => ({
+//       transform: {
+//         experimentalImportSupport: false,
+//         inlineRequires: true,
+//       },
+//     }),
+//   },
+//   resolver: {
+//     sourceExts: process.env.RN_SRC_EXT
+//       ? [...process.env.RN_SRC_EXT.split(',').concat(defaultSourceExts), 'cjs'] // <-- cjs added here
+//       : [...defaultSourceExts, 'cjs'], // <-- cjs added here
+//   },
+// };
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Button, Center, Text } from 'native-base';
 import { useFindUsersQuery } from '../../types';
 import { useAuth } from '../contexts/Auth';
+import { Image, View, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export const HomeScreen = () => {
+  const [image, setImage] = useState(null);
   const { data, error, loading } = useFindUsersQuery();
   const { signOut } = useAuth();
+
+  const openImagePickerAsync = async () => {
+    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert('Permission to access camera roll is required!');
+      return;
+    }
+
+    const pickerResult = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setImage(pickerResult.uri);
+  };
 
   if (loading) {
     return <Center>Loading...</Center>;
@@ -29,6 +54,11 @@ export const HomeScreen = () => {
           ))}
         </Box>
       </Center>
+
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button onPress={openImagePickerAsync}> Pick an Image </Button>
+        {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      </View>
     </Box>
   );
 };
